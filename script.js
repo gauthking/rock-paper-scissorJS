d317a8a39581e55667f934cb5cef31a7329f2183
Gautham <appskans2017@gmail.com>
2022-03-29 14:57:50 +0530

    fixed JS issues

--- script.js
+++ script.js
@@ -13,20 +13,20 @@ function playRound(playerSelection, computerSelection) {
     let x = playerSelection;
     if (computerSelection == "rock") {
         if (x == "paper") {
-            document.getElementById("roundscore").innerText = "Paper beats rock, You get 1 point!";
+            document.getElementById("roundscore").innerHTML = "Computer Selection : Rock<br>Player Selection : Paper<br> Paper Beats Rock. You get 1 point! ";
             document.getElementById("roundscore").style.display = "block";
             console.log("Paper beats rock, You get 1 point!");
             score += 1;
         }
 
         else if (x == computerSelection) {
-            document.getElementById("roundscore").innerText = "Draw no point..";
+            document.getElementById("roundscore").innerHTML = "Computer Selection : Rock<br>Player Selection : Rock<br> Draw! No points ";
             document.getElementById("roundscore").style.display = "block";
             console.log("Draw no point..");
         }
 
         else {
-            document.getElementById("roundscore").innerText = "You lose. Rock beats" + " " + x;
+            document.getElementById("roundscore").innerHTML = "Computer Selection : Rock<br>Player Selection : Scissors<br> Rock Beats Scissors. You get 0 point! ";
             document.getElementById("roundscore").style.display = "block";
             console.log("You lose. Rock beats" + " " + x);
         }
@@ -34,20 +34,20 @@ function playRound(playerSelection, computerSelection) {
 
     else if (computerSelection == "paper") {
         if (x == "scissors") {
-            document.getElementById("roundscore").innerText = "Scissors beats paper, You get 1 point!";
+            document.getElementById("roundscore").innerHTML = "Computer Selection : Paper<br>Player Selection : Scissors<br> Scissors Beats Paper. You get 1 point! ";
             document.getElementById("roundscore").style.display = "block";
             console.log("Scissors beats paper, You get 1 point!");
             score += 1;
         }
 
         else if (x == computerSelection) {
-            document.getElementById("roundscore").innerText = "Draw no point..";
+            document.getElementById("roundscore").innerHTML = "Computer Selection : Paper<br>Player Selection : Paper<br> Draw! No points ";
             document.getElementById("roundscore").style.display = "block";
             console.log("Draw no point..")
         }
 
         else {
-            document.getElementById("roundscore").innerText = "You lose. Paper beats" + " " + x;
+            document.getElementById("roundscore").innerHTML = "Computer Selection : Paper<br>Player Selection : Rock<br> Paper Beats Rock. You get 0 point! ";
             document.getElementById("roundscore").style.display = "block";
             console.log("You lose. Paper beats" + " " + x);
         }
@@ -55,20 +55,20 @@ function playRound(playerSelection, computerSelection) {
 
     else if (computerSelection == "scissors") {
         if (x == "rock") {
-            document.getElementById("roundscore").innerText = "Rock beats scissors, You get 1 point!";
+            document.getElementById("roundscore").innerHTML = "Computer Selection : Scissors<br>Player Selection : Rock<br> Rock Beats Scissors. You get 1 point! ";
             document.getElementById("roundscore").style.display = "block";
             console.log("Rock beats scissors, You get 1 point!");
             score += 1;
         }
 
         else if (x == computerSelection) {
-            document.getElementById("roundscore").innerText = "Draw no point..";
+            document.getElementById("roundscore").innerHTML = "Computer Selection : Scissors<br>Player Selection : Scissors<br> Draw! No points ";
             document.getElementById("roundscore").style.display = "block";
             console.log("Draw no point..");
         }
 
         else {
-            document.getElementById("roundscore").innerText = "You lose. Scissors beats" + " " + x;
+            document.getElementById("roundscore").innerHTML = "Computer Selection : Scissors<br>Player Selection : Paper<br> Scissors Beats Paper. You get 0 point! ";
             document.getElementById("roundscore").style.display = "block";
             console.log("You lose. Scissors beats" + " " + x);
         }
